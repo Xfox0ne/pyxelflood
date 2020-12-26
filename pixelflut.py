9e41aaac256133056a69fc7ca484e0ef3d6cd521
Benedikt Gruber <53448898+DeadPi@users.noreply.github.com>
2020-12-26 01:04:49 +0100

    Create pixelflut.py

---
+++ pixelflut.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+from PIL import Image
+
+HOST = '127.0.0.1'
+PORT = 8080
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+send = sock.send
+
+xoffset = int(input("X Offset: "))
+yoffset = int(input("Y Offset: "))
+
+xmotion = int(input("X Motion: "))
+ymotion = int(input("Y Motion: "))
+
+im = Image.open('image.jpg')
+rgb_im = im.convert('RGB')
+width = rgb_im.size[0]
+heigth = rgb_im.size[1]
+
+def pixel(x,y,r,g,b,a=255):
+  if a == 255:
+    send(b'PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
+  else:
+    send(b'PX %d %d %02x%02x%02x%02x\n' % (x,y,r,g,b,a))
+
+while True:
+    for i in range(0, width):
+        for j in range(0, heigth):
+            r, g, b = rgb_im.getpixel((i, j))
+            x = threading.Thread(target=pixel, args=(i + xoffset, j + yoffset, r, g, b), daemon=True)
+            x.start()
+    xoffset += xmotion
+    yoffset += ymotion
