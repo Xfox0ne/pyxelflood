108e424c57c3ba4c8c4ac5903f71e03168c32733
Benedikt Gruber <53448898+TFox42@users.noreply.github.com>
2020-12-30 19:47:13 +0100

    Update pixelflut.py

--- pixelflut.py
+++ pixelflut.py
@@ -4,9 +4,6 @@ from PIL import Image
 
 HOST = 'box.pixel-competition.de'
 PORT = 2342
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-send = sock.send
 
 xoffset = int(input("X Offset: "))
 yoffset = int(input("Y Offset: "))
@@ -20,6 +17,10 @@ width = rgb_im.size[0]
 heigth = rgb_im.size[1]
 
 def pixel(x,y,r,g,b,a=255):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect((HOST, PORT))
+  send = sock.send
+
   if a == 255:
     send(b'PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
   else:
@@ -27,13 +28,19 @@ def pixel(x,y,r,g,b,a=255):
 
 threadList = []
 
-for i in range(0, width):
-    for j in range(0, heigth):
-        r, g, b = rgb_im.getpixel((i, j))
-        x = threading.Thread(target=pixel, args=(i + xoffset, j + yoffset, r, g, b), daemon=True)
-        x.start()
-        threadList.append(x)
+while(True):
+  for i in range(0, width):
+      for j in range(0, heigth):
+          r, g, b = rgb_im.getpixel((i, j))
+          x = threading.Thread(target=pixel, args=(i + xoffset, j + yoffset, r, g, b), daemon=True)
+          x.start()
+          threadList.append(x)
 
-while True:
-    for thread in threadList:
-      thread.join()
+      for thread in threadList:
+        thread.join()
+      threadList = []
+
+
+#while True:
+#    for thread in threadList:
+#      thread.join()
