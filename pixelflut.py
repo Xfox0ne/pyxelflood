8af42eba267e8d831ea36b0cabf3de6cbb7d722c
Benedikt Gruber <53448898+TFox42@users.noreply.github.com>
2020-12-30 22:22:50 +0100

    Update pixelflut.py

--- pixelflut.py
+++ pixelflut.py
@@ -15,29 +15,35 @@ width = rgb_im.size[0]
 heigth = rgb_im.size[1]
 
 def pixel(command):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect((HOST, PORT))
-  send = sock.send
+  while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    send = sock.send
 
-  send(b'%s' %(command))
+    send(b'%s' % (command))
 
 stringList = []
 
+for x in range(0, threadcount):
+  stringList.append("")
+
+x = 0
 for i in range(0, width):
-  stringX = ""
   for j in range(0, heigth):
       r, g, b = rgb_im.getpixel((i, j))
-      stringX.join('PX %d %d %02x%02x%02x\n' % (i,j,r,g,b))
-  stringList.append(stringX)
+      stringList[x].join('PX %d %d %02x%02x%02x\n' % (i,j,r,g,b))
+      x += 1
+      if(x > threadcount):
+        x = 0
 
+threadList = []
 
-while True:
-  for i in range(0, len(stringList)):
-    threadList = []
-    for x in range(0, threadcount):
-      t = threading.Thread(target=pixel, args=stringList[i], daemon=True)
-      t.start()
-      threadList.append(t)
+for x in range(0, threadcount):
+  t = threading.Thread(target=pixel, args=stringList[x], daemon=True)
+  t.start()
+  threadList.append(t)
 
-    for thread in threadList:
-      thread.join()
+for thread in threadList:
+  thread.join()
+
+    
